3974eb9dd6bcb21add13cd851ba5a83d60f5c1f9
PierreSQS <nomo_messina@web.de>
2022-05-08 23:03:42 +0200

    created integration test (alt2)

--- src/test/java/guru/springframework/controllers/IndexControllerPierrotTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerPierrotTest.java
@@ -9,6 +9,7 @@ import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.Model;
@@ -26,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * Created by Pierrot on 8/05/22.
  */
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(controllers = IndexController.class)
 public class IndexControllerPierrotTest {
 
